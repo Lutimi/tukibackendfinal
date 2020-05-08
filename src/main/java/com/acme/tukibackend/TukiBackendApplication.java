2dc1b4722e760f516619ab5ef608c49b48f59bcb
Richard <richard.cancinov@gmail.com>
2020-05-07 22:40:34 -0500

    [FEATURE-01] Start the project with settings based in Spring and Maven

---
+++ src/main/java/com/acme/tukibackend/TukiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.tukibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/tukibackend/TukiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.tukibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
